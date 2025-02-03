c74ddd7a8a8a4320fd48a75e759dd3460046ec04
C8C-MMVI <raivenchristian.navor@lorma.edu>
2025-02-03 09:18:39 +0800

    Added new repository; new program Coffee; new object class CoffeeDescription

---
+++ src/main/java/com/coffeemenu/coffeeinfo/Coffee.java
@@ -0,0 +1,4 @@
+package com.coffeemenu.coffeeinfo;
+
+public class Coffee {
+}
---
+++ src/main/java/com/coffeemenu/coffeeinfo/CoffeeDescription.java
@@ -0,0 +1,14 @@
+package com.coffeemenu.coffeeinfo;
+
+public class CoffeeDescription {
+    String name;
+    String type;
+    String size;
+    int price;
+    String roastLevel;
+    String origin;
+    boolean isDecaf;
+    boolean stock;
+    String flavorNotes;
+    String brewMethod;
+}
